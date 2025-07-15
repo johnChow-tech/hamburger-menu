e1dbdc013b6678afa637dc4b4224423032b1e82e
johnChow-tech <shulking@hotmail.com>
2025-07-15 13:52:03 +0900

    feat: add main function of folding and expanding menu

--- js/main.js
+++ js/main.js
@@ -1,17 +1,37 @@
-document.addEventListener("DOMContentLoaded", () => {});
+document.addEventListener("DOMContentLoaded", () => {
+  initHamburgerBtn();
+  initSubMenu();
+});
 
-const hamburgerBtnClass = ".hamburger-btn";
-const naviLinksClass = ".navi-links";
+// Mostly use vars
+const isOpen = "is-open";
+
+const hamburgerBtnToggle = ".js-hamburger-btn-toggle";
+const naviLinksToggle = ".js-navi-links-toggle";
+const hasSubMenuToggle = ".js-navi-link-has-submenu-toggle";
+const subMenuToggle = ".js-sub-menu-toggle";
+
+const hamburgerBtn = document.querySelector(hamburgerBtnToggle);
+const naviLinks = document.querySelector(naviLinksToggle);
+const hasSubMenu = document.querySelector(hasSubMenuToggle);
+const subMenu = document.querySelector(subMenuToggle);
 /**
  * 初始化汉堡菜单按钮
  * @param null
  */
 function initHamburgerBtn() {
-  const btn = document.getElementsByClassName(hamburgerBtnClass);
-  const links = document.getElementsByClassName(naviLinksClass);
-  const isOpen = "is-open";
+  hamburgerBtn.addEventListener("click", () => {
+    naviLinks.classList.toggle(isOpen);
+    if (subMenu.classList.contains(isOpen)) {
+      subMenu.classList.remove(isOpen);
+    }
+  });
+}
 
+function initSubMenu() {
+  const btn = document.querySelector(hasSubMenuToggle);
+  const display = document.querySelector(subMenuToggle);
   btn.addEventListener("click", () => {
-    links.classList.toggle(isOpen);
+    display.classList.toggle(isOpen);
   });
 }
